96919a84bbbce5bcd17473ffdd6c91e44d274f2d
chrism116 <dchristophermorgan@gmail.com>
2019-01-31 21:31:25 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(p)
+  if p==2
+    true
+  elsif p>1
+    for n in 2..p-1 do
+      if p%n==0
+        return false
+      end
+
+    end
+    return true
+  else
+    return false
+  end
+end
